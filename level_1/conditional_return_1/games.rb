74dd281d32ebdabe42f3b7decd910970e67e2e1f
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-12-06 17:34:39 -0200

    Refactored to fill search result with condition answer

--- level_1/conditional_return_1/games.rb
+++ level_1/conditional_return_1/games.rb
@@ -3,9 +3,9 @@
 search = "Contra"
 games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
 search_index = games.find_index(search)
-if search_index
-  search_result = "Game #{search} found: #{games[search_index]} at index #{search_index}."
+search_result = if search_index
+   "Game #{search} found: #{games[search_index]} at index #{search_index}."
 else
-  search_result = "Game #{search} not found."
+  "Game #{search} not found."
 end
 puts search_result
