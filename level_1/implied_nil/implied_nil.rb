dc7d7bb16c122d9d9c32b2cd87bcbb8be06b32bd
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-12-06 17:02:18 -0200

    Refactored to have inline conditionals also implied nil

--- level_1/implied_nil/implied_nil.rb
+++ level_1/implied_nil/implied_nil.rb
@@ -4,8 +4,5 @@
 search = "Contra"
 games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
 search_index = games.find_index(search)
-if search_index != nil
-  puts "Game #{search} Found: #{games[search_index]} at index #{search_index}."
-else
-  puts "Game #{search} not found."
-end
+puts "Game #{search} Found: #{games[search_index]} at index #{search_index}." if search_index
+puts "Game #{search} not found." unless search_index
