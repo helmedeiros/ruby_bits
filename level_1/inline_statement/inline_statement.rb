4934d7b5cdbed9761cc5155ff495f06eac91f814
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-12-06 16:31:41 -0200

    Refactored to have a inline statement

--- level_1/inline_statement/inline_statement.rb
+++ level_1/inline_statement/inline_statement.rb
@@ -1,6 +1,4 @@
 # Inline Statements
 # Doing a full unless statement is sometimes too much. Refactor the method below to use a single-line unless statement.
 games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
-unless games.empty?
-  puts "Games in your vast collection: #{games.count}"
-end
+puts "Games in your vast collection: #{games.count}" unless games.empty?
