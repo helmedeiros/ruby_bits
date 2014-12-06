11452d258c05bc70104810cb81d4b39afe0ac3df
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-12-06 17:56:38 -0200

    Solved the problem using short circuit evaluation

--- level_1/short_circuit_evaluation/game.rb
+++ level_1/short_circuit_evaluation/game.rb
@@ -2,13 +2,7 @@
 # Using Short-Circuit Evaluation can clean up your code a great deal.
 # Update the following method to use short circuit evaluation. While you're at it, why not try reducing the entire method to one line?
 def search_index(games, search_term)
-  search_index = games.find_index(search_term)
-
-  if search_index
-    search_index
-  else
-    "Not Found"
-  end
+  games.find_index(search_term) || "Not Found"
 end
 
 games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
