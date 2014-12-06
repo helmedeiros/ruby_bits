c83b706d459dc5f92c3155dfe2cabcbbb4655f64
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-12-06 17:14:14 -0200

    Used the short circuit and to group if statements

--- level_1/short_circuit_and/games.rb
+++ level_1/short_circuit_and/games.rb
@@ -6,8 +6,6 @@ games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
 matched_games = games.grep(Regexp.new(search))
 
 # Found an exact match
-if matched_games.length > 0
-  if matched_games.include?(search)
+if matched_games.length > 0 && matched_games.include?(search)
     puts "Game #{search} found."
-  end
 end
