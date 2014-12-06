6b4868b5c23e7410f02185d9a3908f24811270b1
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-12-06 17:29:52 -0200

    Solved problem using the ||= conditional assignment fot search

--- level_1/conditional_assignment/gamers.rb
+++ level_1/conditional_assignment/gamers.rb
@@ -1,7 +1,7 @@
 # Conditional Assignment
 # If no search is entered, we'll display all games. Notice the first line below where we're setting search to an empty string?
 # Change this to use conditional assignment.
-search = "" unless search
+search ||= ""
 games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
 matched_games = games.grep(Regexp.new(search))
 puts "Found the following games..."
