df8ea70ca7b6367724883edf3982957156638956
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-02-07 07:15:40 -0200

    solve problem creating a new class

--- level_2/5_class/game.rb
+++ level_2/5_class/game.rb
@@ -1,4 +1,15 @@
 class Game
+  attr_accessor :name, :system, :year
   def initialize(name, options={})
+    @name = name
+    @system = options[:system]
+    @year = options[:year]
+  end
+
+  def format
+    [@name, @system, @year].compact.join(', ')
   end
 end
+
+street_fight = Game.new("Street Fight", { :system => "PS3", :year => "2014"})
+puts street_fight.format
