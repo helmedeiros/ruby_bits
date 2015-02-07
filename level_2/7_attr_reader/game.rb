d4b2087eab98006957015025fda209079a4b629f
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-02-07 07:58:22 -0200

    add created_at with current time also add a getter for it avoiding to be changed

--- level_2/7_attr_reader/game.rb
+++ level_2/7_attr_reader/game.rb
@@ -5,5 +5,14 @@ class Game
     @name = name
     @year = options[:year]
     @system = options[:system]
+    @created_at = Time.new
+  end
+
+  def created_at
+    @created_at
   end
 end
+
+street_fight = Game.new('Street Fight', {:year => 2014, :system => 'PS3'})
+puts street_fight.name
+puts street_fight.created_at
