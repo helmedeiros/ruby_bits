f0f25e991077303ef34b405312d644a2fc1120f8
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-02-07 07:48:18 -0200

    solve problem using attr_accessor

--- level_2/6_attr_accessor/game.rb
+++ level_2/6_attr_accessor/game.rb
@@ -1,19 +1,14 @@
 class Game
+  attr_accessor :name, :year, :system
   def initialize(name, options={})
     @name = name
     @year = options[:year]
     @system = options[:system]
   end
-
-  def name
-    @name
-  end
-
-  def year
-    @year
-  end
-
-  def system
-    @system
-  end
 end
+
+street_fight = Game.new("Street Fight", {:year => 2104, :system => 'PS3'})
+
+puts street_fight.name
+puts street_fight.year
+puts street_fight.system
