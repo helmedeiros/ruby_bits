3843e4c01969570504028d3d2e8c3232ad1887b6
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-03-04 09:41:54 -0300

    solved using active_support array.from

---
+++ level_4/1_arrays/game.rb
@@ -0,0 +1,8 @@
+  require 'active_support/all'
+
+  def last_games(games, index)
+    games.from(index)
+  end
+
+  games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
+  puts last_games(games, 1)
