0003491a4348989885bca601092ac39a7cfdf746
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-03-06 09:44:35 -0300

    solve date problem using advance function

---
+++ level_4/3_dates/game.rb
@@ -0,0 +1,12 @@
+require 'active_support/all'
+
+def anniversary(game, years)
+  game[:release].advance(years: years)
+end
+
+game = {
+  name: 'Contra',
+  release: DateTime.new(1987, 2, 20, 0, 0, 0)
+}
+
+puts anniversary(game, 20)
