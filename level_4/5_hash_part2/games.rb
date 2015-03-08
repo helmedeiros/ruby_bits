69eae8686e9e41e4d58d24ae6040b3f8156a0d49
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-03-07 21:10:57 -0300

    solved hash problems using ActiveSupport except method

---
+++ level_4/5_hash_part2/games.rb
@@ -0,0 +1,12 @@
+require 'active_support/all'
+
+def exclude_character(games, character)
+  games.except(character);
+end
+
+games = {
+  mario: ["Super Mario World", "Super Smash Bros. Melee"],
+  luigi: ["Luigi's Mansion"],
+  yoshi: ["Yoshi's Island", "Yoshi's Story"]
+}
+puts exclude_character(games, :yoshi)
