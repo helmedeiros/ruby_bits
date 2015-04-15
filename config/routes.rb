3d5db69a01f52edd2bb5ecec76a72e4a3ecd5df7
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2015-04-15 13:09:00 -0300

    feat: surface lesson activities and add submissions route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   resources :tracks, only: [:index, :show] do
     resources :modules, only: [:show], controller: 'learning_modules' do
-      resources :lessons, only: [:show]
+      resources :lessons, only: [:show] do
+        resources :activities, only: [] do
+          resources :submissions, only: [:create]
+        end
+      end
     end
   end
 end
