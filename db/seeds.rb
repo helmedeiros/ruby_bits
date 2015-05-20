cf5db1e869b9869cb3e74815f5a5a4be7e5109b9
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2015-05-20 10:24:00 -0300

    feat(seeds): seed badge catalogue from YAML

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
 require File.expand_path('../seeds/loader', __FILE__)
 Seeds::Loader.new(Rails.root.join('db', 'seeds')).call
+
+# Badge catalogue
+require "yaml"
+file = Rails.root.join("db/seeds/badges.yml")
+if file.exist?
+  YAML.load_file(file).each do |row|
+    Badge.find_or_create_by!(slug: row["slug"]) do |b|
+      b.name = row["name"]; b.description = row["description"]; b.icon_glyph = row["icon_glyph"]
+    end
+  end
+end
