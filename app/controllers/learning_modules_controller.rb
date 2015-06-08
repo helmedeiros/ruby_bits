1efd206807fe669f039ec499e1e6d3c361c031e7
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2015-06-08 11:19:00 -0300

    feat: surface per-lesson progress on module page via LessonPresenter

--- app/controllers/learning_modules_controller.rb
+++ app/controllers/learning_modules_controller.rb
@@ -2,6 +2,7 @@ class LearningModulesController < ApplicationController
   def show
     @track   = Track.find_by!(slug: params[:track_id])
     @module  = @track.modules.find_by!(slug: params[:id])
-    @lessons = @module.lessons
+    @lessons = @module.lessons.includes(:activities)
+    @presented = @lessons.map { |l| LessonPresenter.new(l, current_user) }
   end
 end
